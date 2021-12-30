b8f0f96d232e8e952a92f1bfd1f288a16ab4ce5d
JianLiu1994 <709501841@qq.com>
2021-12-29 22:09:51 -0600

    updated structure based templates search pipeline

--- bml_casp15/complex_templates_search/structure_based_pipeline.py
+++ bml_casp15/complex_templates_search/structure_based_pipeline.py
@@ -6,13 +6,6 @@ from bml_casp15.common.util import makedir_if_not_exists, check_dirs
 import pandas as pd
 from multiprocessing import Pool
 
-def loadFastaDictionary(dict_file):
-    fasta_dict = {}
-    with open(dict_file, "r") as f:
-        for line in f:
-            fasta_dict[line.strip().split(":")[0].strip()] = line.strip().split(":")[1].strip()
-    return fasta_dict
-
 
 def read_pair_file_into_array(_input):
     output_array = []
@@ -54,16 +47,25 @@ def tmscore_file_reader(gen_dir):
         for line in contents:
             line = line.rstrip()
             if "Aligned length=" in line:
-                contents = item.split(",")
-                aligned_lenght = contents[0].strip().split("=")[1]
-                rmsd = contents[1].strip().split("=")[1]
-                id = contents[2].strip().split("=")[2]
+                line_contents = line.split(",")
+                aligned_length = line_contents[0].split("=")[1].strip()
+                rmsd = line_contents[1].strip().split("=")[1]
+                id = line_contents[2].strip().split("=")[2].strip()
             if "(if normalized by length of Chain_1)" in line:
                 line = line[0:line.index("(if normalized by length of Chain_1)")].strip()
                 tmscore = line.split("=")[1].strip()
-        aln_temp = contents[-3].rstrip()
-        aln_query = contents[-1].rstrip()
-        return [os.path.basename(gen_dir).split(".")[0].strip(), tmscore, id, aligned_lenght, aln_temp, aln_query]
+        aln_temp = contents[-2].rstrip()
+        aln_query = contents[-4].rstrip()
+        return {'template': os.path.basename(gen_dir).split(".")[0].strip(),
+                'tmscore': tmscore,
+                'seqid': id,
+                'aligned_length': aligned_length,
+                'aln_temp': aln_temp,
+                'tstart': 1,
+                'tend': len(aln_temp.replace("-", "")),
+                'aln_query': aln_query,
+                'qstart': 1,
+                'qend': len(aln_query.replace("-", ""))}
     return None
 
 
@@ -78,7 +80,6 @@ def only_protein_names(_input):
 
 
 def tmalign(inparams):
-
     model1, model2, tmalign_program, logdir = inparams
 
     modelname = os.path.basename(model2)
@@ -110,7 +111,7 @@ class Complex_structure_based_template_search_pipeline:
 
         self.params = params
 
-    def search(self, monomers, outdir, is_homodimer = False):
+    def search(self, monomers, outdir, is_homodimer=False):
 
         outdir = os.path.abspath(outdir) + "/"
 
@@ -124,13 +125,11 @@ class Complex_structure_based_template_search_pipeline:
 
         makedir_if_not_exists(outdir)
 
-        os.chdir(self.params['complex_atom_dir'])
-
-        print(self.params['complex_atom_dir'])
-
         all_scores = {}
 
         for i in range(len(monomers)):
+
+            os.chdir(self.params['complex_atom_dir'])
 
             monomer = monomers[i]
 
@@ -141,15 +140,16 @@ class Complex_structure_based_template_search_pipeline:
                 all_scores[i] = copy.deepcopy(temp_score)
 
             else:
-                tm_score_dir = f'{outdir}/tmscore/{monomer}/'
+                tm_score_dir = f'{outdir}/tmscore/{os.path.basename(monomer)}/'
                 makedir_if_not_exists(tm_score_dir)
 
-                aln_score_dir = f'{outdir}/aln/{monomer}/'
+                aln_score_dir = f'{outdir}/aln/{os.path.basename(monomer)}/'
                 makedir_if_not_exists(aln_score_dir)
 
                 process_list = []
                 for pdb in pdbs_to_compare:
-                    if not os.path.exists(f"{tm_score_dir}/{pdb}.atom.txt"):
+                    if not os.path.exists(f"{tm_score_dir}/{pdb}.atom.txt") or len(
+                            open(f"{tm_score_dir}/{pdb}.atom.txt").readlines()) == 0:
                         process_list.append([monomer, pdb + '.atom', self.params['tmalign_program'], tm_score_dir])
 
                 pool = Pool(processes=10)
@@ -159,21 +159,17 @@ class Complex_structure_based_template_search_pipeline:
 
                 txt_files = specific_filters(tm_score_dir)
 
-                temp_score = []
+                temp_score = {}
                 for proteins in txt_files:
                     val = tmscore_file_reader(proteins)
-                    print(val)
-                    len_val = len(val)
-                    if len(val) > 0 and float(val[1]) > float(self.params["tmalign_threshold"]):
-                        template_name = os.path.basename(proteins).split(".")[0]
+                    if len(val) > 0:# and float(val[1]) > float(self.params["tmalign_threshold"]):
                         # aln_out = aln_output_formatter(_query=val[len_val - 1], _query_name=current_pdb_name,
                         #                                _aligned_len=val[len_val - 3], _temp_name=template_name,
                         #                                _temp=val[len_val - 2])
-                        temp_score.append(val)
+                        temp_score[val['template']] = val
                         # write2file(current_aln_dir + template_name + ".pir", aln_out)
 
                 all_scores[i] = temp_score
-
 
         dimer_to_be_compare = []
         with open(dimer_list) as f:
@@ -183,23 +179,39 @@ class Complex_structure_based_template_search_pipeline:
                 dimer_to_be_compare += [f"{chain1}_{chain2}"]
                 dimer_to_be_compare += [f"{chain2}_{chain1}"]
 
-
         temp_scores = []
         for dimer in dimer_to_be_compare:
-
             chain1, chain2 = dimer.split('_')
 
-            chain1_tmscore = all_scores[0]['chain1']
-            chain2_tmscore = all_scores[1]['chain2']
+            chain1_tmscore = all_scores[0][chain1]['tmscore']
+            chain2_tmscore = all_scores[1][chain2]['tmscore']
+            avg_score = (float(chain1_tmscore) + float(chain2_tmscore)) / 2
 
-            avg_score = (float(chain1_tmscore) + float(chain2_tmscore))/2
+            aln_temp1 = all_scores[0][chain1]['aln_temp']
+            tstart1 = all_scores[0][chain1]['tstart']
+            tend1 = all_scores[0][chain1]['tend']
 
-            temp_scores += [chain1, chain2, chain1_tmscore, chain2_tmscore, avg_score, min(chain1_tmscore, chain2_tmscore),
-                            max(chain1_tmscore, chain2_tmscore)]
+            aln_query1 = all_scores[0][chain1]['aln_query']
+            qstart1 = all_scores[0][chain1]['qstart']
+            qend1 = all_scores[0][chain1]['qend']
 
-        temp_scores_sorted = sorted(temp_scores, key=lambda energy: float(energy[4]), reverse=True)
+            aln_temp2 = all_scores[1][chain2]['aln_temp']
+            tstart2 = all_scores[1][chain2]['tstart']
+            tend2 = all_scores[1][chain2]['tend']
 
-        df = pd.DataFrame(temp_scores_sorted, columns=['chain1', 'chain2', 'tmscore1', 'tmscore2', 'avg_tmscore', 'min_tmscore', 'max_tmscore'], dtype=float)
+            aln_query2 = all_scores[1][chain2]['aln_query']
+            qstart2 = all_scores[1][chain2]['qstart']
+            qend2 = all_scores[1][chain2]['qend']
 
-        df.to_csv(outdir + "/result_contact_" + str(self.params["tmalign_threshold"]) + ".csv")
+            temp_scores.append([chain1, chain2, avg_score,
+                               chain1_tmscore, aln_temp1, tstart1, tend1, aln_query1, qstart1, qend1,
+                               chain2_tmscore, aln_temp2, tstart2, tend2, aln_query2, qstart2, qend2])
 
+        temp_scores_sorted = sorted(temp_scores, key=lambda energy: float(energy[2]), reverse=True)
+
+        df = pd.DataFrame(temp_scores_sorted[0:int(self.params['template_count'])],
+                          columns=['chain1', 'chain2', 'avg_tmscore',
+                                   'tmscore1', 'aln_temp1', 'tstart1', 'tend1', 'aln_query1', 'qstart1', 'qend1',
+                                   'tmscore2', 'aln_temp2', 'tstart2', 'tend2', 'aln_query2', 'qstart2', 'qend2'])
+
+        df.to_csv(outdir + "/structure_based_templates.csv", index=False)
