1990baa7b10f85cace27b304a18e0a7394b908ee
jl4mc <jl4mc@sunflower.eecs.missouri.edu>
2022-01-12 21:38:50 -0600

    check monomer result

--- bin/N3_generate_monomer_tertiary_structure.py
+++ bin/N3_generate_monomer_tertiary_structure.py
@@ -6,11 +6,20 @@ from bml_casp15.tertiary_structure_generation.pipeline import *
 from absl import flags
 from absl import app
 
-
 flags.DEFINE_string('option_file', None, 'option file')
 flags.DEFINE_string('aln_dir', None, 'Monomer alignment directory')
 flags.DEFINE_string('output_dir', None, 'Output directory')
 FLAGS = flags.FLAGS
+
+
+def complete_result(outputdir):
+    complete = True
+    for i in range(0, 5):
+        model = f'{outputdir}/ranked_{i}.pdb'
+        if not os.path.exists(model):
+            complete = False
+            break
+    return complete
 
 
 def main(argv):
@@ -29,10 +38,9 @@ def main(argv):
     fasta_paths = []
 
     for monomer in os.listdir(FLAGS.aln_dir):
-
         if os.path.exists(f"{FLAGS.aln_dir}/{monomer}/{monomer}.fasta"):
-
-            fasta_paths += [f"{FLAGS.aln_dir}/{monomer}/{monomer}.fasta"]
+            if not complete_result(FLAGS.output_dir + '/' + monomer):
+                fasta_paths += [f"{FLAGS.aln_dir}/{monomer}/{monomer}.fasta"]
 
     print(f"Total {len(fasta_paths)} monomers are generating structures")
 
