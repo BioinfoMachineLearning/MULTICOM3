6dcbeeaad21ae70292cff713f421e8fdd2cd5206
jl4mc <jl4mc@aster.engineering.missouri.edu>
2022-02-28 14:28:41 -0600

    keep the first model in iterative pipeline

--- bml_casp15/quaternary_structure_generation/iterative_search_pipeline.py
+++ bml_casp15/quaternary_structure_generation/iterative_search_pipeline.py
@@ -655,6 +655,17 @@ class Multimer_iterative_generation_pipeline:
                     ref_start_pkl = f"result_model_{max_index + 1}_multimer.pkl"
                     print('##################################################')
                 else:
+                    # keep the models in iteration 1 even through the plddt score decreases
+                    if num_iteration == 1:
+                        ref_avg_lddt = 0
+                        with open(ref_start_pkl, 'rb') as f:
+                            prediction_result = pickle.load(f)
+                            ref_avg_lddt = np.mean(prediction_result['plddt'])
+                        ref_tmscore = 0
+                        if os.path.exists(native_pdb):
+                            ref_tmscore = _cal_tmscore(self.params['tmscore_program'], ref_start_pdb, native_pdb)
+                        model_iteration_scores += [ref_avg_lddt]
+                        model_iteration_tmscores += [ref_tmscore]
                     break
 
             # model_iteration_scores += [max_lddt_score]
