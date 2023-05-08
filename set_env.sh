adb71c16fd028a5bc150be923b4580d6eff05b67
Jian Liu <709501841@qq.com>
2023-05-07 22:53:43 -0500

    Delete set_env.sh

--- set_env.sh
+++
@@ -1,5 +0,0 @@
-conda config --add channels defaults
-conda config --add channels bioconda
-conda config --add channels conda-forge
-
-conda install -y -c bioconda hmmer==3.3.2 hhsuite==3.3.0 kalign2==2.04
